39dedb0404f0e4af83ba9656baadf073bba2745b
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-12-02 22:31:23 -0300

    Initing castle game
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>

---
+++ src/Ball.js
@@ -0,0 +1,58 @@
+class Ball {
+  constructor(x, y, weight, value) {
+    this.x = x;
+    this.y = y;
+    this.weight = weight;
+    this.value = value;
+  }
+
+/*   collide() {
+    for (let i = this.id + 1; i < numBalls; i++) {
+      // console.log(others[i]);
+      let dx = this.others[i].x - this.x;
+      let dy = this.others[i].y - this.y;
+      let distance = sqrt(dx * dx + dy * dy);
+      let minDist = this.others[i].diameter / 2 + this.diameter / 2;
+      //   console.log(distance);
+      //console.log(minDist);
+      if (distance < minDist) {
+        //console.log("2");
+        let angle = atan2(dy, dx);
+        let targetX = this.x + cos(angle) * minDist;
+        let targetY = this.y + sin(angle) * minDist;
+        let ax = (targetX - this.others[i].x) * spring;
+        let ay = (targetY - this.others[i].y) * spring;
+        this.vx -= ax;
+        this.vy -= ay;
+        this.others[i].vx += ax;
+        this.others[i].vy += ay;
+      }
+    }
+  } */
+
+/*   move() {
+    this.vy += gravity;
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x + this.diameter / 2 > width) {
+      this.x = width - this.diameter / 2;
+      this.vx *= friction;
+    } else if (this.x - this.diameter / 2 < 0) {
+      this.x = this.diameter / 2;
+      this.vx *= friction;
+    }
+    if (this.y + this.diameter / 2 > height) {
+      this.y = height - this.diameter / 2;
+      this.vy *= friction;
+    } else if (this.y - this.diameter / 2 < 0) {
+      this.y = this.diameter / 2;
+      this.vy *= friction;
+    }
+  } */
+
+  display() {
+    noStroke();
+    fill(0, 204);
+    ellipse(this.x, this.y, 80);
+  }
+}
